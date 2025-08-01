7fb1afa645f9636a068d6562a3f5d1ed5aa5cf22
Omar Salas <51163778+OmyyFlow@users.noreply.github.com>
2025-07-31 21:33:41 -0600

    Create FFD_BPP.cpp
    INICIAL COMM

---
+++ FFD_BPP.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int FFD(int p[], int n, int c);
+
+int main(){
+  int p[] = {1, 2, 9, 6, 5, 4, 7, 1, 3, 8}; //Cargas
+  int c = 10; //Capacidad de cada bin
+  int n = sizeof(weight) / sizeof(weight[0]);//cant de objetos
+
+  
+  return 0;
+}
