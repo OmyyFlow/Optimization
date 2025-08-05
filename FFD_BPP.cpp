1e7554b2fe6f1aeafa29810d8079699133903bbb
Omar Salas <51163778+OmyyFlow@users.noreply.github.com>
2025-08-05 17:03:03 -0600

    Update FFD_BPP.cpp

--- FFD_BPP.cpp
+++ FFD_BPP.cpp
@@ -2,12 +2,27 @@
 #include <stdlib.h>
 
 int FFD(int p[], int n, int c);
+void mergesort
 
 int main(){
   int p[] = {1, 2, 9, 6, 5, 4, 7, 1, 3, 8}; //Cargas
   int c = 10; //Capacidad de cada bin
   int n = sizeof(weight) / sizeof(weight[0]);//cant de objetos
 
+  printf("Cantidad de bins usados en la instancia: %d", FDD(p,n,c));
   
   return 0;
 }
+
+int FFD(int p[], int n, int c){
+  int cant_bins = 0;
+  int cargas[n];
+
+  for(int i=0, i<n, i++){
+    cargas[i] = c;
+  }
+
+  
+
+  return cant_bins;
+}  
